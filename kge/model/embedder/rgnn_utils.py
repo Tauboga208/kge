7401a9a692d700a3785b9ad970a8aa66889d3c72
Tabea Bucher <tabea.bucher@web.de>
2021-10-11 22:06:39 +0200

    move helper functions to separate utils script

---
+++ kge/model/embedder/rgnn_utils.py
@@ -0,0 +1,170 @@
+from math import floor, sqrt
+import random
+import torch
+from typing import Optional, Tuple
+
+# ---- Message Passing Helper Functions ---- #
+
+# TODO referencen woher (CompGCN special ding)
+def broadcast(src: torch.Tensor, other: torch.Tensor, dim: int):
+    if dim < 0:
+        dim = other.dim() + dim
+    if src.dim() == 1:
+        for _ in range(0, dim):
+            src = src.unsqueeze(0)
+    for _ in range(src.dim(), other.dim()):
+        src = src.unsqueeze(-1)
+    src = src.expand_as(other)
+    return src
+
+def scatter_sum(src: torch.Tensor, index: torch.Tensor, dim: int = -1,
+                out: Optional[torch.Tensor] = None,
+                dim_size: Optional[int] = None) -> torch.Tensor:
+    index = broadcast(index, src, dim)
+    if out is None:
+        size = list(src.size())
+        if dim_size is not None:
+            size[dim] = dim_size
+        elif index.numel() == 0:
+            size[dim] = 0
+        else:
+            size[dim] = int(index.max()) + 1
+        out = torch.zeros(size, dtype=src.dtype, device=src.device)
+        return out.scatter_add_(dim, index, src)
+    else:
+        return out.scatter_add_(dim, index, src)
+
+
+def scatter_add(src: torch.Tensor, index: torch.Tensor, dim: int = -1,
+                out: Optional[torch.Tensor] = None,
+                dim_size: Optional[int] = None) -> torch.Tensor:
+    return scatter_sum(src, index, dim, out, dim_size)
+
+
+def scatter_mul(src: torch.Tensor, index: torch.Tensor, dim: int = -1,
+                out: Optional[torch.Tensor] = None,
+                dim_size: Optional[int] = None) -> torch.Tensor:
+    return torch.ops.torch_scatter.scatter_mul(src, index, dim, out, dim_size)
+
+
+def scatter_mean(src: torch.Tensor, index: torch.Tensor, dim: int = -1,
+                 out: Optional[torch.Tensor] = None,
+                 dim_size: Optional[int] = None) -> torch.Tensor:
+
+    out = scatter_sum(src, index, dim, out, dim_size)
+    dim_size = out.size(dim)
+
+    index_dim = dim
+    if index_dim < 0:
+        index_dim = index_dim + src.dim()
+    if index.dim() <= index_dim:
+        index_dim = index.dim() - 1
+
+    ones = torch.ones(index.size(), dtype=src.dtype, device=src.device)
+    count = scatter_sum(ones, index, index_dim, None, dim_size)
+    count[count < 1] = 1
+    count = broadcast(count, out, dim)
+    if out.is_floating_point():
+        out.true_divide_(count)
+    else:
+        out.floor_divide_(count)
+    return out
+
+
+def scatter_min(
+        src: torch.Tensor, index: torch.Tensor, dim: int = -1,
+        out: Optional[torch.Tensor] = None,
+        dim_size: Optional[int] = None) -> Tuple[torch.Tensor, torch.Tensor]:
+    return torch.ops.torch_scatter.scatter_min(src, index, dim, out, dim_size)
+
+
+def scatter_max(
+        src: torch.Tensor, index: torch.Tensor, dim: int = -1,
+        out: Optional[torch.Tensor] = None,
+        dim_size: Optional[int] = None) -> Tuple[torch.Tensor, torch.Tensor]:
+    return torch.ops.torch_scatter.scatter_max(src, index, dim, out, dim_size)
+
+
+def scatter(src: torch.Tensor, index: torch.Tensor, dim: int = -1,
+            out: Optional[torch.Tensor] = None, dim_size: Optional[int] = None,
+            reduce: str = "sum") -> torch.Tensor:
+    if reduce == 'sum' or reduce == 'add':
+        return scatter_sum(src, index, dim, out, dim_size)
+    if reduce == 'mul':
+        return scatter_mul(src, index, dim, out, dim_size)
+    elif reduce == 'mean':
+        return scatter_mean(src, index, dim, out, dim_size)
+    elif reduce == 'min':
+        return scatter_min(src, index, dim, out, dim_size)[0]
+    elif reduce == 'max':
+        return scatter_max(src, index, dim, out, dim_size)[0]
+    else:
+        raise ValueError
+
+def scatter_(name, src, index, dim_size=None):
+	r"""Aggregates all values from the :attr:`src` tensor at the indices
+	specified in the :attr:`index` tensor along the first dimension.
+	If multiple indices reference the same location, their contributions
+	are aggregated according to :attr:`name` (either :obj:`"add"`,
+	:obj:`"mean"` or :obj:`"max"`).
+
+	Args:
+		name (string): The aggregation to use (:obj:`"add"`, :obj:`"mean"`,
+			:obj:`"max"`).
+		src (Tensor): The source tensor.
+		index (LongTensor): The indices of elements to scatter.
+		dim_size (int, optional): Automatically create output tensor with size
+			:attr:`dim_size` in the first dimension. If set to :attr:`None`, a
+			minimal sized output tensor is returned. (default: :obj:`None`)
+
+	:rtype: :class:`Tensor`
+	"""
+	if name == 'add': name = 'sum'
+	assert name in ['sum', 'mean', 'max']
+	out = scatter(src, index, dim=0, out=None, dim_size=dim_size, reduce=name)
+	return out[0] if isinstance(out, tuple) else out
+
+# ---- RGCN-specific Weight Initialisation Methods ---- #
+
+# initialisation methods adapted from the R-GCN PyTorch Implementation:
+# https://arxiv.org/pdf/2107.10015.pdf
+
+def schlichtkrull_std(tensor, gain, shape=None):
+    """
+    a = \text{gain} \times \frac{3}{\sqrt{\text{fan\_in} + \text{fan\_out}}}
+    """
+    if shape: 
+        fan_in, fan_out = shape[0], shape[1]
+    else:
+        fan_in, fan_out = torch.nn.init._calculate_fan_in_and_fan_out(tensor)
+    return gain * 3.0 / sqrt(float(fan_in + fan_out))
+
+def schlichtkrull_normal_(tensor, gain=1., shape=None):
+    """Fill the input `Tensor` with values according to the Schlichtkrull method, using a normal distribution."""
+    std = schlichtkrull_std(tensor, gain, shape)
+    with torch.no_grad():
+        return tensor.normal_(0.0, std)
+
+def schlichtkrull_uniform_(tensor, gain=1., shape=None):
+    """Fill the input `Tensor` with values according to the Schlichtkrull method, using a uniform distribution."""
+    std = schlichtkrull_std(tensor, gain, shape)
+    with torch.no_grad():
+        return tensor.uniform_(-std, std)
+
+# ---- Composition Functions for Message Passing ---- #
+
+def neighbor(h_i, h_j, h_r):
+    return h_j
+
+def sub(h_i, h_j, h_r):
+    return h_j-h_r   
+
+def mult(h_i, h_j, h_r):
+    return h_j*h_r   
+
+def ccorr(h_i, h_j, h_r):
+    return torch.irfft(
+        com_mult(conj(torch.rfft(h_j, 1)), torch.rfft(h_r, 1)),
+        1, 
+        signal_sizes=(h_r.shape[-1],)
+    )  
